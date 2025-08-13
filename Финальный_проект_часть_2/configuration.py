1c9926ce4c64e4761f1eaa1f216842b160dcfcf9
Sergei Timofeev <sa.timofeev@yahoo.com>
2025-08-13 10:17:12 -0600

    was added autotests

---
+++ Финальный_проект_часть_2/configuration.py
@@ -0,0 +1,7 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+URL_SERVICE = "https://2dcea980-a0f7-45e3-8944-7fab628990fe.serverhub.praktikum-services.ru"
+
+#ручка создания заказа
+CREATE_ORDER = "/api/v1/orders"
+#ручка Получить заказ по его номеру
+GET_TRACK = "/api/v1/orders/track"
